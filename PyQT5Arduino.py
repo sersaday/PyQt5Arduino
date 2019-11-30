5b1584eaaf3815a6c5c8c85ac751acd40cbb7002
Serhat Saday <sersaday@hotmail.com>
2019-11-30 17:14:10 +0300

    Update PyQT5Arduino.py

--- PyQT5Arduino.py
+++ PyQT5Arduino.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'pythonblue.ui'
+# Form implementation generated from reading ui file 'PyQt5Arduino.ui'
 #
 # Created by: PyQt5 UI code generator 5.13.0
 #
